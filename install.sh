737125107f8d1729d41272cfa286599cea5127df
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2022-08-03 20:53:14 +0000

    install

---
+++ install.sh
@@ -0,0 +1,12 @@
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
+google-chrome --version
+rm google-chrome-stable_current_amd64.deb
+
+wget https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+sudo chown root:root /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
+rm chromedriver_linux64.zip
